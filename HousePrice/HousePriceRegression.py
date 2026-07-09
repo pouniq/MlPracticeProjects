8f1278b98002a4d9196f5141a36e4ae9a3ffc2e5
pouniq <abdollahpora@gmail.com>
2026-07-09 22:53:52 +0330

    price regression some progress

---
+++ HousePrice/HousePriceRegression.py
@@ -0,0 +1,60 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+from sklearn.model_selection import KFold, train_test_split, cross_validate, GridSearchCV
+from sklearn.pipeline import Pipeline
+from sklearn.compose import ColumnTransformer # for handling categorical values
+from sklearn.preprocessing import StandardScaler, OneHotEncoder
+from sklearn.impute import SimpleImputer
+
+
+from sklearn.linear_model import LinearRegression, Ridge, Lasso
+from sklearn.ensemble import RandomForestRegressor, HistGradientBoostingRegressor
+
+from sklearn.metrics import (
+    mean_squared_error,
+    mean_absolute_error,
+    root_mean_squared_error,
+    r2_score
+)
+
+
+# config.
+pd.set_option('display.max_columns', None)
+pd.set_option('display.float_format', lambda x: f'{x:.3f}')
+sns.set_theme(style='darkgrid')
+
+plt.rcParams.update(
+    {
+        'axes.titlesize': 10,
+        'axes.labelsize': 9,
+        'xtick.labelsize': 8,
+        'ytick.labelsize': 8
+    }
+)
+
+RANDOM_STATE = 42
+CSV_PATH = 'housing.csv' 
+TARGET_COL = 'median_house_value'
+
+
+df = pd.read_csv(CSV_PATH)
+
+df.shape
+df.describe()
+df.info()
+df.value_counts(normalize=True)
+df.columns
+# target column: median_house_value
+
+
+
+df.groupby(TARGET_COL).value_counts()
+
+num_cols = df.select_dtypes(include= [np.number]).columns.tolist()
+cat_cols = df.select_dtypes(include = ['object']).columns.tolist()
+
+
+
